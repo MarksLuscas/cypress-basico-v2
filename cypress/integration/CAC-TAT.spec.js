c93137fdf8bfccb484a79349e2e3ddc89e3dd4fc
Lucas Marques <lucas99marques@gmail.com>
2024-07-03 22:24:40 -0300

    subindo novo testCase

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -9,7 +9,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.title().should("be.equal", "Central de Atendimento ao Cliente TAT")
     })
 
-        it('preenche os campos obrigatórios e envia os formulários', function () {
+        it('preenche os campos obrigatórios e envia os formulários - ex 1', function () {
             const textoLongo = "texto texto texto texto texto texto texto texto texto texto texto texto texto"
             cy.get('#firstName').type("Lucas")
             cy.get('#lastName').type("Marques")
@@ -19,7 +19,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
             cy.get('.success > strong').should('be.visible')
         })
 
-        it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function() {
+        it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida - ex 2', function() {
             cy.get('#firstName').type("Lucas")
             cy.get('#lastName').type("Marques")
             cy.get('#email').type("lucas.teste.com")
@@ -28,11 +28,18 @@ describe('Central de Atendimento ao Cliente TAT', function() {
             cy.get('.error').should('be.visible') 
         })
 
-        it('validando campo de telefone continua vazio apos passar valor não-numerico', function() {
+        it('validando campo de telefone continua vazio apos passar valor não-numerico - ex 3', function() {
             cy.get('#phone').should('be.empty')
 
             cy.get('#phone').type('aaasd')
 
             cy.get('#phone').should('be.empty')
+        })
+
+        it("seleciona um produto (YouTube) por seu texto", function () {
+            cy.get('#product').select("youtube")
+
+            cy.get('#product').should('have.value', 'youtube')
+
         })
   })
